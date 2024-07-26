75c4f077a6312805d2d625435a0dfdf6dd4b0d1f
HlibPavlyk <pavlikgo78@gmail.com>
2024-07-26 20:04:16 +0300

    feat: add banking components ro dependency container

--- src/MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
+++ src/MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -1,4 +1,9 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repositories;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infrastructure.Bus;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,5 +14,10 @@ public class DependencyContainer
     public static void RegisterServices(IServiceCollection services)
     {
         services.AddTransient<IEventBus, RabbitMqBus>();
+        
+        services.AddTransient<IAccountService, AccountService>();
+        
+        services.AddTransient<IAccountRepository, AccountRepository>();
+        services.AddTransient<BankingDbContext>();
     }
 }
