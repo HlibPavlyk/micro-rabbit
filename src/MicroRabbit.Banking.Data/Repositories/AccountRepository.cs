c4eedba44cdd6bacef963bb0fb8217c82e9dbdc5
HlibPavlyk <pavlikgo78@gmail.com>
2024-07-26 19:50:44 +0300

    feat: implement banking data layer

---
+++ src/MicroRabbit.Banking.Data/Repositories/AccountRepository.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Entities;
+using MicroRabbit.Banking.Domain.Interfaces;
+
+namespace MicroRabbit.Banking.Data.Repositories;
+
+public class AccountRepository : IAccountRepository
+{
+    private readonly BankingDbContext _context;
+
+    public AccountRepository(BankingDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Account> GetAccounts()
+    {
+        return _context.Accounts;
+    }
+}
---
+++ src/MicroRabbit.Banking.Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,8 @@
+﻿using MicroRabbit.Banking.Domain.Entities;
+
+namespace MicroRabbit.Banking.Domain.Interfaces;
+
+public interface IAccountRepository
+{
+    IEnumerable<Account> GetAccounts();
+}
