7650bead63dbf6873415fac7143eb7a6f971b55b
HlibPavlyk <pavlikgo78@gmail.com>
2024-07-27 12:45:00 +0300

    feat: config transfer startup file
    - add necessary packages

--- src/MicroRabbit.Transfer.Api/Program.cs
+++ src/MicroRabbit.Transfer.Api/Program.cs
@@ -1,8 +1,22 @@
+using MediatR;
+using MicroRabbit.Infrastructure.IoC;
+using MicroRabbit.Transfer.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TransferDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TransferDbConnection"));
+});
+
+builder.Services.AddMediatR(typeof(Program).Assembly);
+builder.Services.RegisterServices();
 
 var app = builder.Build();
 
