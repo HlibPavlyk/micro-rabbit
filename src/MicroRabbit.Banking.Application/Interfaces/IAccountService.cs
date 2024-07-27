3637ac7d916af7f014b38fed3f50db2c0719deff
HlibPavlyk <pavlikgo78@gmail.com>
2024-07-27 11:34:42 +0300

    feat: add banking transfer commands
    - remove .http unnecessary file

--- src/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
+++ src/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
@@ -1,8 +1,10 @@
-﻿using MicroRabbit.Banking.Domain.Entities;
+﻿using MicroRabbit.Banking.Application.Dtos;
+using MicroRabbit.Banking.Domain.Entities;
 
 namespace MicroRabbit.Banking.Application.Interfaces;
 
 public interface IAccountService
 {
     IEnumerable<Account> GetAccounts();
+    void Transfer(AccountTransfer accountTransfer);
 }
---
+++ src/MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Domain.Core.Bus;
+
+namespace MicroRabbit.Banking.Domain.CommandHandlers;
+
+public class CreateTransferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+{
+    private readonly IEventBus _bus;
+
+    public CreateTransferCommandHandler(IEventBus bus)
+    {
+        _bus = bus;
+    }
+
+    public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(true);
+    }
+}
