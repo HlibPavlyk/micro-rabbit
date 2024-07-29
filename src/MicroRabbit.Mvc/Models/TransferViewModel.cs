4a633f4f4974972767334ff9815e7bb384ed179c
HlibPavlyk <pavlikgo78@gmail.com>
2024-07-29 14:08:27 +0300

    fix: update view form

--- src/MicroRabbit.Mvc/Models/TransferViewModel.cs
+++ src/MicroRabbit.Mvc/Models/TransferViewModel.cs
@@ -1,11 +1,17 @@
-﻿using System.Runtime.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace MicroRabbit.Mvc.Models;
 
 public class TransferViewModel
 {
-    public string Notes { get; set; }
+    [Required]
     public int FromAccount { get; set; }
+    
+    [Required]
     public int ToAccount { get; set; }
+    
+    [Required]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
     public decimal TransferAmount { get; set; }
 }
